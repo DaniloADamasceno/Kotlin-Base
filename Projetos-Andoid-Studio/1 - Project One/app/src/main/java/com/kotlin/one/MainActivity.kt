38701543434571b7cb00cb49690cde613a2a86a7
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2023-09-03 11:28:44 -0300

    :pencil2:Project Two --> Volta do commit '85bbc49F' para algumas alterações e inclusões de recursos de load de imagens

---
+++ Projetos-Andoid-Studio/1 - Project One/app/src/main/java/com/kotlin/one/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.kotlin.one
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }, 5000)
+
+    }
+}
---
+++ Projetos-Andoid-Studio/2 - Project Two/app/src/main/java/com/kotlin/two/LogoMainActivity.kt
@@ -0,0 +1,23 @@
+package com.kotlin.two
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.kotlin.two.R
+
+class LogoMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo_main)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this@LogoMainActivity, LoginPageActivity::class.java)
+            startActivity(intent)
+        }, 5000)
+    }
+}
